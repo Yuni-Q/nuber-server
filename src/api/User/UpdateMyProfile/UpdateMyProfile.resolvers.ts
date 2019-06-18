72c9e6f1988fa3ddf4222bba7ccc63a01e3fed50
Yuni-Q <carnotcycle2@naver.com>
2019-06-18 20:50:50 +0900

    add 1.57

--- src/api/User/UpdateMyProfile/UpdateMyProfile.resolvers.ts
+++ src/api/User/UpdateMyProfile/UpdateMyProfile.resolvers.ts
@@ -1,14 +1,43 @@
 import User from "src/entities/User";
-import { UpdateMyProfileMutationArgs } from "src/types/graph";
+import {
+  UpdateMyProfileMutationArgs,
+  UpdateMyProfileResponse
+} from "src/types/graph";
+import cleanNullArgs from "src/utils/cleanNullArgs";
 import privateResolver from "src/utils/privateResolver";
 import { Resolvers } from "../../../types/resolvers";
 
 const resolvers: Resolvers = {
   Mutation: {
-    UpdateMyProfile: privateResolver( async(_, args: UpdateMyProfileMutationArgs, {req}) => {
-      const user: User = req.user;
-      await User.update({id: user.id}, {..args})
-    })
+    UpdateMyProfile: privateResolver(
+      async (
+        _,
+        args: UpdateMyProfileMutationArgs,
+        { req }
+      ): Promise<UpdateMyProfileResponse> => {
+        const user: User = req.user;
+        const notNull: any = cleanNullArgs(args); // 👈🏻 Add ':any'
+        // ⚠️ Take the if(notNull.password) OUT of the try/catch
+        if (notNull.password !== null) {
+          // 👈🏻 Change from args to notNull
+          user.password = notNull.password; // 👈🏻 Same here
+          user.save();
+          delete notNull.password; // <--  ⚠️⚠️⚠️ Delete password  from notNull or is going to be saved again without encoding ⚠️⚠️⚠️
+        }
+        try {
+          await User.update({ id: user.id }, { ...notNull });
+          return {
+            ok: true,
+            error: null
+          };
+        } catch (error) {
+          return {
+            ok: false,
+            error: error.message
+          };
+        }
+      }
+    )
   }
 };
 
