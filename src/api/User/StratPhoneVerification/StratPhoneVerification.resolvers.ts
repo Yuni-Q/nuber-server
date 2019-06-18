995690fa5ed4a8bf2ca068770c9e15cc4259179e
Yuni-Q <carnotcycle2@naver.com>
2019-06-18 19:57:46 +0900

    fix: fix StratPhoneVerification

--- src/api/User/StratPhoneVerification/StratPhoneVerification.resolvers.ts
+++ src/api/User/StratPhoneVerification/StratPhoneVerification.resolvers.ts
@@ -1,33 +1,40 @@
-
 import Verification from "../../../entities/Verification";
-import { StratPhoneVerificationMutationArgs, StratPhoneVerificationResponse } from "../../../types/graph";
+import {
+  StratPhoneVerificationMutationArgs,
+  StratPhoneVerificationResponse
+} from "../../../types/graph";
 import { Resolvers } from "../../../types/resolvers";
 
 const resolvers: Resolvers = {
   Mutation: {
-    StratPhoneVerification: async (_, args: StratPhoneVerificationMutationArgs): Promise<StratPhoneVerificationResponse> => {
+    StratPhoneVerification: async (
+      _,
+      args: StratPhoneVerificationMutationArgs
+    ): Promise<StratPhoneVerificationResponse> => {
       const { phoneNumber } = args;
       try {
-        const existingVerification = await Verification.findOne({payload: phoneNumber});
-        if ( existingVerification) {
+        const existingVerification = await Verification.findOne({
+          payload: phoneNumber
+        });
+        if (existingVerification) {
           await existingVerification.remove();
         }
         const newVerifcation = await Verification.create({
           payload: phoneNumber,
-          target: "PHONE",
+          target: "PHONE"
         }).save();
         // TODO: send sms
         // await sendVerificationSMS(newVerifcation.payload, newVerifcation.key);
         console.log(newVerifcation);
         return {
           ok: true,
-          error: null,
-        }
+          error: newVerifcation.key
+        };
       } catch (error) {
         return {
           ok: false,
-          error: error.message,
-        }
+          error: error.message
+        };
       }
     }
   }
