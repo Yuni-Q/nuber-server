78696d5755acb3cd72da4d7f924944dbe8bad837
Yuni-Q <carnotcycle2@naver.com>
2019-06-19 11:55:30 +0900

    change pc

--- src/api/User/GetNearbyDrivers/GetNearbyDrivers.resolvers.ts
+++ src/api/User/GetNearbyDrivers/GetNearbyDrivers.resolvers.ts
@@ -1,37 +1,35 @@
-import { Between, getRepository } from "typeorm";
+import { Between, getRepository } from 'typeorm';
 
-import User from "src/entities/User";
-import { GetNearbyDriversRespose } from "src/types/graph";
-import { Resolvers } from "src/types/resolvers";
-import privateResolver from "src/utils/privateResolver";
+import User from '../../../entities/User';
+import { GetNearbyDriversRespose } from '../../../types/graph';
+import { Resolvers } from '../../../types/resolvers';
+import privateResolver from '../../../utils/privateResolver';
 
 const resolvers: Resolvers = {
-  Query: {
-    GetNearbyDrivers: privateResolver(
-      async (_, __, { req }): Promise<GetNearbyDriversRespose> => {
-        const user: User = req.user;
-        const { lastLat, lastLng } = user;
-        try {
-          const drivers: User[] = await getRepository(User).find({
-            isDriving: true,
-            lastLat: Between(lastLat - 0.05, lastLat + 0.05),
-            lastLng: Between(lastLng - 0.05, lastLng + 0.05)
-          });
-          return {
-            ok: true,
-            error: null,
-            drivers
-          };
-        } catch (error) {
-          return {
-            ok: false,
-            error: error.message,
-            drivers: null
-          };
-        }
-      }
-    )
-  }
+	Query: {
+		GetNearbyDrivers: privateResolver(async (_, __, { req }): Promise<GetNearbyDriversRespose> => {
+			const user: User = req.user;
+			const { lastLat, lastLng } = user;
+			try {
+				const drivers: User[] = await getRepository(User).find({
+					isDriving: true,
+					lastLat: Between(lastLat - 0.05, lastLat + 0.05),
+					lastLng: Between(lastLng - 0.05, lastLng + 0.05)
+				});
+				return {
+					ok: true,
+					error: null,
+					drivers
+				};
+			} catch (error) {
+				return {
+					ok: false,
+					error: error.message,
+					drivers: null
+				};
+			}
+		})
+	}
 };
 
 export default resolvers;
